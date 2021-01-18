52fb79278e3b5699a22ec5128e1f8d868279faa0
Rafael Siqueira <rafael.siqueira@onesight.com.br>
2021-01-18 18:35:14 -0300

    Add API fetch on Consumption and History

--- components/Consumption.js
+++ components/Consumption.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text} from 'react-native';
+import {api} from '../api';
 
 const Consumption = () => {
-  return <Text>Consumption</Text>;
+  const [history, setHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    api.get('/usage/packageInformation/').then((res) => {
+      setHistory(res.data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  return <Text>{JSON.stringify(history, null, 4)}</Text>;
 };
 
 export default Consumption;
--- components/History.js
+++ components/History.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text} from 'react-native';
+import {api} from '../api';
 
 const History = () => {
-  return <Text>History</Text>;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [startDate, setStartDate] = useState('2020-08-01');
+  const [endDate, setEndDate] = useState('2020-08-03');
+
+  useEffect(() => {
+    api
+      .get(`/usage/records/?startDate=${startDate}&endDate=${endDate}`)
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      });
+  }, [startDate, endDate]);
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  return <Text>{JSON.stringify(data, null, 4)}</Text>;
 };
 
 export default History;
