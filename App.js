a6ff08b336e708fb62d5344a6c4157e5641c6b3b
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2021-01-19 19:09:05 -0300

    Add icons to navigation bar

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -13,6 +5,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import Faq from './components/Faq';
 import Consumption from './components/Consumption';
 import History from './components/History';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Root = createBottomTabNavigator();
 
@@ -22,9 +15,48 @@ const App = () => {
       <StatusBar barStyle="dark-content" />
       <NavigationContainer>
         <Root.Navigator>
-          <Root.Screen name="Consumo instantâneo" component={Consumption} />
-          <Root.Screen name="Histórico de consumo" component={History} />
-          <Root.Screen name="Perguntas Frequentes" component={Faq} />
+          <Root.Screen
+            name="Consumo instantâneo"
+            component={Consumption}
+            options={{
+              tabBarLabel: 'Consumo instantâneo',
+              tabBarIcon: ({color, size}) => (
+                <MaterialCommunityIcons
+                  name="chart-donut"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+          <Root.Screen
+            name="Histórico de consumo"
+            component={History}
+            options={{
+              tabBarLabel: 'Histórico de consumo',
+              tabBarIcon: ({color, size}) => (
+                <MaterialCommunityIcons
+                  name="history"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+          <Root.Screen
+            name="Perguntas Frequentes"
+            component={Faq}
+            options={{
+              tabBarLabel: 'Perguntas Frequentes',
+              tabBarIcon: ({color, size}) => (
+                <MaterialCommunityIcons
+                  name="frequently-asked-questions"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
         </Root.Navigator>
       </NavigationContainer>
     </>
