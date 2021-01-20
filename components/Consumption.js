4aae875cded9dd4521b22b78ba1cc1abdb4f8e35
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2021-01-20 08:19:10 -0300

    Add layout to Consumption component

--- components/Consumption.js
+++ components/Consumption.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {api} from '../api';
 import Loading from './Loading';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Consumption = () => {
   const [history, setHistory] = useState([]);
@@ -19,16 +20,63 @@ const Consumption = () => {
   }
 
   return (
-    <ScrollView style={styles.container}>
-      <Text>{JSON.stringify(history, null, 4)}</Text>
-    </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.strong}>
+          Dados contratados no início do ciclo atual
+        </Text>
+        <Ionicons name="analytics-outline" style={styles.icon} />
+        <Text style={styles.text}>{history.subscription} MB</Text>
+      </View>
+      <View style={styles.card}>
+        <Text style={styles.strong}>
+          Dados adicionais contratados durante o ciclo atual
+        </Text>
+        <Ionicons name="analytics-outline" style={styles.icon} />
+        <Text style={styles.text}>{history.topup} MB</Text>
+      </View>
+      <View style={styles.card}>
+        <Text style={styles.strong}>Bônus recebidos durante o ciclo atual</Text>
+        <Ionicons name="analytics-outline" style={styles.icon} />
+        <Text style={styles.text}>{history.bonus} MB</Text>
+      </View>
+      <View style={styles.card}>
+        <Text style={styles.strong}>Dados disponíveis para consumo</Text>
+        <Ionicons name="analytics-outline" style={styles.icon} />
+        <Text style={styles.text}>{history.available} MB</Text>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 12,
+    paddingTop: 12,
+    paddingHorizontal: 12,
+    paddingBottom: 4,
     flex: 1,
+    justifyContent: 'space-between',
+  },
+  card: {
+    borderColor: '#ccc',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    flex: 1,
+    padding: 8,
+    marginVertical: 4,
+    justifyContent: 'space-between',
+  },
+  strong: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  text: {
+    textAlign: 'center',
+  },
+  icon: {
+    textAlign: 'center',
+    fontSize: 24,
   },
 });
 
